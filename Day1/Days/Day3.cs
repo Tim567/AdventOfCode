da32b8b0fe8dd33f58f3102af783555c82e2d27d
Tim Bentum <tim@adjust-it.nl>
2022-12-03 10:40:00 +0100

    P1

--- Day1/Days/Day3.cs
+++ Day1/Days/Day3.cs
@@ -17,8 +17,55 @@ namespace Day1.Days
         public void Run()
         {
             string[] data = GetInput();
+            int total = 0;
             // implement solution
-            
+            foreach (var rucksack in data)
+            {
+                //split rucksack into half
+                string left = rucksack.Substring(0, rucksack.Length / 2);
+                string right = rucksack.Substring(rucksack.Length / 2);
+                //split into inivitual chars
+                char[] leftChars = left.ToCharArray();
+                char[] rightChars = right.ToCharArray();
+
+                //check what chars are in both
+                int count = 0;
+                //List<Char> both = new List<char>();
+                Dictionary<Char, int> both = new Dictionary<char, int>();
+                foreach (var leftChar in leftChars)
+                {
+                    if (rightChars.Contains(leftChar))
+                    {
+                        count++;
+                        if (both.ContainsKey(leftChar))
+                        {
+                            both[leftChar]++;
+                        }
+                        else
+                        {
+                            both.Add(leftChar, 1);
+                        }
+                    }
+                }
+                //Lowercase item types a through z have priorities 1 through 26.
+                //Uppercase item types A through Z have priorities 27 through 52.
+                //The priority of an item is the sum of the priorities of its characters.
+                int priority = 0;
+                foreach (var item in both)
+                {
+                    if (item.Key >= 'a' && item.Key <= 'z')
+                    {
+                        priority += item.Key - 'a' + 1;
+                    }
+                    else if (item.Key >= 'A' && item.Key <= 'Z')
+                    {
+                        priority += item.Key - 'A' + 27;
+                    }
+                }
+                Console.WriteLine($"Count: {count}, Priority: {priority}");
+                total += priority;
+            }
+            Console.WriteLine($"Total: {total}");
         }
     }
 }
