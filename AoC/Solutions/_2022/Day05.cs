ba798d71b993dc55cf63282a9267b94bb403622a
Tim Bentum <s1158990@student.windesheim.nl>
2022-12-06 22:55:46 +0100

    Add visualisation for day 5 :)

--- AoC/Solutions/_2022/Day05.cs
+++ AoC/Solutions/_2022/Day05.cs
@@ -9,6 +9,8 @@ namespace AoC.Solutions._2022
         List<(int cnt, int src, int dest)> instructions;
         public Day05() => Seed();
 
+        int defCurY = 0;
+
         private void Seed()
         {
             data = GetInput();
@@ -25,20 +27,40 @@ namespace AoC.Solutions._2022
         public override void RunPart1()
         {
             Seed();
-            foreach (var ins in instructions) for (int i = 0; i < ins.cnt; i++) stacks[ins.dest - 1].Push(stacks[ins.src - 1].Pop());
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Do you want to visualize the process? (y/n)");
+            Console.ForegroundColor = ConsoleColor.White;
+            bool visualize = Console.ReadKey().Key == ConsoleKey.Y;
+            defCurY = Console.CursorTop;
+            foreach (var ins in instructions)
+            {
+                for (int i = 0; i < ins.cnt; i++)
+                {
+                    stacks[ins.dest - 1].Push(stacks[ins.src - 1].Pop());
+                    if (visualize) VisualizeCurrentStacks();
+                }
+
+            };
             StringBuilder sb = new();
             foreach (var s in stacks) sb.Append(s.Peek());
+            //VisualizeCurrentStacks();
             Console.WriteLine("Answer Part 1: " + sb);
         }
 
         public override void RunPart2()
         {
             Seed();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Do you want to visualize the process? (y/n)");
+            Console.ForegroundColor = ConsoleColor.White;
+            bool visualize = Console.ReadKey().Key == ConsoleKey.Y;
+            defCurY = Console.CursorTop;
             foreach (var ins in instructions)
             {
                 Stack<char> tmp = new Stack<char>();
                 for (int i = 0; i < ins.cnt; i++) tmp.Push(stacks[ins.src - 1].Pop());
                 while (tmp.Count > 0) stacks[ins.dest - 1].Push(tmp.Pop());
+                if (visualize) VisualizeCurrentStacks();
             }
             StringBuilder sb = new();
             foreach (var s in stacks) sb.Append(s.Peek());
@@ -69,6 +91,41 @@ namespace AoC.Solutions._2022
             }
 
             return crates;
+        }
+
+        void VisualizeCurrentStacks()
+        {
+            // Visualize current stacks like:
+            //  [N] [Q] [N]
+            //  [R] [F] [Q] [G] [M]
+            //  [J] [Z] [T] [R] [H] [J]
+            //  [T] [H] [G] [R] [B] [N] [T]
+            //  [Z] [J] [J] [G] [F] [Z] [S] [M]
+            //  [B] [N] [N] [N] [Q] [W] [L] [Q] [S]
+            //  [D] [S] [R] [V] [T] [C] [C] [N] [G]
+            //  [F] [R] [C] [F] [L] [Q] [F] [D] [P]
+            //  1   2   3   4   5   6   7   8   9
+
+            Thread.Sleep(1);
+            
+            var stacksCopy = new List<Stack<char>>();
+            foreach (var s in stacks) stacksCopy.Add(new Stack<char>(s.Reverse()));
+            Console.SetCursorPosition(0, defCurY);
+
+            //int highestStackCount = stacksCopy.Select(s => s.Count).Max();
+            int highestStackCount = 35;
+            for (int i = 0; i < highestStackCount; i++)
+            {
+                StringBuilder sb = new();
+                for (int j = 0; j < stacksCopy.Count; j++)
+                {
+                    if (highestStackCount - i <= stacksCopy[j].Count) sb.Append($"[{stacksCopy[j].Pop()}] ");
+                    else sb.Append("    ");
+                }
+                Console.WriteLine(sb);
+            }
+            Console.WriteLine(" 1   2   3   4   5   6   7   8   9");
+
         }
 
     }
